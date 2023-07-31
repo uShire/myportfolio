ed2f528113b57ac8d76a16c881b0b4b1edc30056
uShire <afgregana@up.edu.ph>
2023-07-31 14:39:08 +0800

    latest update

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,3 +1,26 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const mobileNav = document.querySelector(".header__nav");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  mobileNav.classList.toggle("open");
+});
+
+// ----------HEADER NAV--------------
+
+const headerNav = document.querySelectorAll(".header__nav ul li");
+headerNav.forEach((header) => {
+  header.addEventListener("click", () => {
+    removeActiveHeader();
+    header.classList.add("active");
+  });
+});
+
+function removeActiveHeader() {
+  headerNav.forEach((header) => {
+    header.classList.remove("active");
+  });
+}
+
 // const header = document.querySelector(".header");
 // window.addEventListener("scroll", () => {
 //   console.log(window.pageYOffset);
@@ -6,13 +29,6 @@
 //   } else {
 //     header.classList.remove("active");
 //   }
-// });
-
-// const toggleMenu = document.querySelector(".toggle__menu");
-// const mobileNav = document.querySelector(".header__actions");
-// toggleMenu.addEventListener("click", () => {
-//   toggleMenu.classList.toggle("open");
-//   mobileNav.classList.toggle("open");
 // });
 
 // const sections = document.querySelectorAll("section");
@@ -37,52 +53,52 @@
 
 // ----------SKILLS--------------
 
-const skillsNav = document.querySelectorAll(".skills__icon__nav");
-const skillsContent = document.querySelectorAll(".language__content__text");
-skillsNav.forEach((skills) => {
-  skills.addEventListener("click", () => {
-    removeActiveSkills();
-    skills.classList.add("active");
-    const activeContent1 = document.querySelector(`#${skills.id}-content`);
-    removeActiveContent1();
-    activeContent1.classList.add("active");
-  });
-});
+// const skillsNav = document.querySelectorAll(".skills__icon__nav");
+// const skillsContent = document.querySelectorAll(".language__content__text");
+// skillsNav.forEach((skills) => {
+//   skills.addEventListener("click", () => {
+//     removeActiveSkills();
+//     skills.classList.add("active");
+//     const activeContent1 = document.querySelector(`#${skills.id}-content`);
+//     removeActiveContent1();
+//     activeContent1.classList.add("active");
+//   });
+// });
 
-function removeActiveSkills() {
-  skillsNav.forEach((skills) => {
-    skills.classList.remove("active");
-  });
-}
+// function removeActiveSkills() {
+//   skillsNav.forEach((skills) => {
+//     skills.classList.remove("active");
+//   });
+// }
 
-function removeActiveContent1() {
-  skillsContent.forEach((skills) => {
-    skills.classList.remove("active");
-  });
-}
+// function removeActiveContent1() {
+//   skillsContent.forEach((skills) => {
+//     skills.classList.remove("active");
+//   });
+// }
 
 // ----------PROJECTS--------------
 
-const projectsNav = document.querySelectorAll(".projects__icon__nav");
-const projectsContent = document.querySelectorAll(".projects__item__wrapper");
-projectsNav.forEach((projects) => {
-  projects.addEventListener("click", () => {
-    removeActiveProjects();
-    projects.classList.add("active");
-    const activeContent2 = document.querySelector(`#${projects.id}-content`);
-    removeActiveContent2();
-    activeContent2.classList.add("active");
-  });
-});
+// const projectsNav = document.querySelectorAll(".projects__icon__nav");
+// const projectsContent = document.querySelectorAll(".projects__item__wrapper");
+// projectsNav.forEach((projects) => {
+//   projects.addEventListener("click", () => {
+//     removeActiveProjects();
+//     projects.classList.add("active");
+//     const activeContent2 = document.querySelector(`#${projects.id}-content`);
+//     removeActiveContent2();
+//     activeContent2.classList.add("active");
+//   });
+// });
 
-function removeActiveProjects() {
-  projectsNav.forEach((projects) => {
-    projects.classList.remove("active");
-  });
-}
+// function removeActiveProjects() {
+//   projectsNav.forEach((projects) => {
+//     projects.classList.remove("active");
+//   });
+// }
 
-function removeActiveContent2() {
-  projectsContent.forEach((projects) => {
-    projects.classList.remove("active");
-  });
-}
+// function removeActiveContent2() {
+//   projectsContent.forEach((projects) => {
+//     projects.classList.remove("active");
+//   });
+// }
